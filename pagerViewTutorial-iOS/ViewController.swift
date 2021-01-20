6a28196a7907834c64cf9d9f4715880d404bf4cc
hyungyu Kim <ypsophila97@icloud.com>
2021-01-20 15:12:21 +0900

    Add pagerView, pagerControl func

--- pagerViewTutorial-iOS/ViewController.swift
+++ pagerViewTutorial-iOS/ViewController.swift
@@ -10,6 +10,8 @@ import FSPagerView
 
 class ViewController: UIViewController,FSPagerViewDataSource,FSPagerViewDelegate {
    
+    @IBOutlet weak var leftBtn: UIButton!
+    @IBOutlet weak var rightBtn: UIButton!
     @IBOutlet weak var myPagerView: FSPagerView! {
         didSet {
             //페이저뷰에 cell 등록
@@ -23,7 +25,14 @@ class ViewController: UIViewController,FSPagerViewDataSource,FSPagerViewDelegate
             }
     }
     
-    @IBOutlet weak var myPagerControl: FSPageControl!
+    @IBOutlet weak var myPagerControl: FSPageControl! {
+        didSet {
+            self.myPagerControl.numberOfPages = self.imageNames.count
+            self.myPagerControl.contentHorizontalAlignment = .center
+            self.myPagerControl.itemSpacing = 16
+            self.myPagerControl.interitemSpacing = 16
+        }
+    }
     
     fileprivate let imageNames = ["1.png","2.png","3.png","4.png"]
 
@@ -33,19 +42,43 @@ class ViewController: UIViewController,FSPagerViewDataSource,FSPagerViewDelegate
         //mypagerView 의 데이터소스와 델리게이트를 설정(FSPagerViewDataSource,FSPagerViewDelegate)
         self.myPagerView.dataSource = self
         self.myPagerView.delegate = self
+        self.leftBtn.layer.cornerRadius = self.leftBtn.frame.height/2
+        self.rightBtn.layer.cornerRadius = self.rightBtn.frame.height/2
+    }
+    //MARK: - IBAction
+    @IBAction func onLeftBtnClicked(_ sender: UIButton) {
+        print("ViewController - onLeftBtnClicked() called")
+        
+    }
+    @IBAction func onRightBtnClicked(_ sender: UIButton) {
+        print("ViewController - onRightBtnClicked() called")
+        
     }
     //MARK: - FSPagerView datasource
     func numberOfItems(in pagerView: FSPagerView) -> Int {
         return imageNames.count
     }
     
+    //각 셀에 대한 설정
     func pagerView(_ pagerView: FSPagerView, cellForItemAt index: Int) -> FSPagerViewCell {
         let cell = pagerView.dequeueReusableCell(withReuseIdentifier: "cell", at: index)
         cell.imageView?.image = UIImage(named: self.imageNames[index])
 //            cell.textLabel?.text =
+        //셀에 들어가는 컨텐트는 꽉차는 것이 아닌 fit 이다.
+        cell.imageView?.contentMode = .scaleAspectFit
             return cell
     }
-
-
+    //MARK: - FSPagerView delegate
+    //수동으로 스와이프할때 pagercontrol 의 인덱스 변경
+    func pagerViewWillBeginDragging(_ pagerView: FSPagerView, targetIndex: Int){
+        self.myPagerControl.currentPage = targetIndex
+    }
+    //자동으로 스와이프할때 pagercontrol 의 인데스 변경
+    func pagerViewDidEndScrollAnimation(_ pagerView: FSPagerView) {
+        self.myPagerControl.currentPage = pagerView.currentIndex
+    }
+    func pagerView(_ pagerView: FSPagerView, shouldHighlightItemAt index: Int) -> Bool {
+        return false
+    }
 }
 
