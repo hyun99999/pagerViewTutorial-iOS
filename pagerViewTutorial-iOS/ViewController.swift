c711d1c2b96cbf7026d868eb348bcce802f58692
hyungyu Kim <ypsophila97@icloud.com>
2021-01-20 11:36:27 +0900

    Install pod

--- pagerViewTutorial-iOS/ViewController.swift
+++ pagerViewTutorial-iOS/ViewController.swift
@@ -6,12 +6,44 @@
 //
 
 import UIKit
+import FSPagerView
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,FSPagerViewDataSource,FSPagerViewDelegate {
+   
+    @IBOutlet weak var myPagerView: FSPagerView! {
+        didSet {
+            //페이저뷰에 cell 등록
+                self.myPagerView.register(FSPagerViewCell.self, forCellWithReuseIdentifier: "cell")
+            //아이템 크기 설정
+            self.myPagerView.itemSize = FSPagerView.automaticSize
+            //무한스크롤 설정
+            self.myPagerView.isInfinite = true
+            //자동스크롤 설정
+            self.myPagerView.automaticSlidingInterval = 3.0
+            }
+    }
+    
+    @IBOutlet weak var myPagerControl: FSPageControl!
+    
+    fileprivate let imageNames = ["1.png","2.png","3.png","4.png"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        //mypagerView 의 데이터소스와 델리게이트를 설정(FSPagerViewDataSource,FSPagerViewDelegate)
+        self.myPagerView.dataSource = self
+        self.myPagerView.delegate = self
+    }
+    //MARK: - FSPagerView datasource
+    func numberOfItems(in pagerView: FSPagerView) -> Int {
+        return imageNames.count
+    }
+    
+    func pagerView(_ pagerView: FSPagerView, cellForItemAt index: Int) -> FSPagerViewCell {
+        let cell = pagerView.dequeueReusableCell(withReuseIdentifier: "cell", at: index)
+        cell.imageView?.image = UIImage(named: self.imageNames[index])
+//            cell.textLabel?.text =
+            return cell
     }
 
 
